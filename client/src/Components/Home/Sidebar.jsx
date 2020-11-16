1c02f0f0704a80fff19bde04ea7936dffb40e7b9
panzerstrike <emesha2030@tutanota.com>
2020-11-17 03:54:46 +0700

    add conditional rendering on sidebar
    Now sidebar will provide menu depend on the role

--- client/src/Components/Home/Sidebar.jsx
+++ client/src/Components/Home/Sidebar.jsx
@@ -14,15 +14,16 @@ function Sidebar() {
   const [state, dispatch] = useContext(LoginContext);
   const id = localStorage.getItem("id");
 
-  const { isLoading, error, data: userData } = useQuery("getImage", () =>
-    API.get(`/user/${id}`)
+  const { isLoading, error, data: userData, refetch } = useQuery(
+    "getImage",
+    () => API.get(`/user/${state.user.id}`)
   );
 
   return isLoading || !userData ? (
     <Spinner />
   ) : error ? (
     <h1>Your Error : {error.message}</h1>
-  ) : (
+  ) : state.user.role === "user" ? (
     <div id="sidebar">
       <div id="userInfo">
         <div id="sidebarProfileImageContainer">
@@ -94,7 +95,7 @@ function Sidebar() {
       <hr />
       <br />
       <div style={{ marginLeft: "25%" }}>
-        <Link className="linkmenu" style={{ textDecoration: "none" }}>
+        <Link to="/" className="linkmenu" style={{ textDecoration: "none" }}>
           <div
             className="buttonmenu-logout"
             onClick={() =>
@@ -111,7 +112,96 @@ function Sidebar() {
         </Link>
       </div>
     </div>
-  );
+  ) : state.user.role === "admin" ? (
+    <div id="sidebar">
+      <div id="userInfo">
+        <div id="sidebarProfileImageContainer">
+          <img
+            className="sidebarProfileImage"
+            src={urlAsset.img + userData.data.data.picture}
+            alt=""
+          />
+        </div>
+        <div className="userInfoName">
+          <h1>{userData.data.data.fullName}</h1>
+        </div>
+      </div>
+
+      <hr />
+      <div style={{ marginLeft: "20%", textAlign: "left", marginTop: "10%" }}>
+        <Link
+          className="linkmenu "
+          style={{ textDecoration: "none" }}
+          to="/profile"
+          // activeClassName="active"
+        >
+          <div className="buttonmenu">
+            <div style={{ marginLeft: "1%", padding: "8%" }}>
+              <FaUser size={24} /> <font className="namamenu"> Profile </font>
+            </div>
+          </div>
+        </Link>
+
+        <Link
+          className="linkmenu"
+          style={{ textDecoration: "none" }}
+          to="/my-library"
+        >
+          <div className="buttonmenu">
+            <div style={{ marginLeft: "1%", padding: "8%" }}>
+              <MdLibraryBooks size={24} />{" "}
+              <font className="namamenu"> My Library </font>
+            </div>
+          </div>
+        </Link>
+
+        <Link
+          className="linkmenu"
+          style={{ textDecoration: "none" }}
+          to="/admin"
+        >
+          <div className="buttonmenu">
+            <div style={{ marginLeft: "1%", padding: "8%" }}>
+              <AiFillFileAdd size={24} />{" "}
+              <font className="namamenu"> Dashboard </font>
+            </div>
+          </div>
+        </Link>
+      </div>
+
+      {/* <div id="sidebarLink">
+        <Link className="link-list" to="/profile">
+          <FaUser size={20} /> <p className="link-text">Profile</p>
+        </Link>
+        <Link className="link-list" to="/profile">
+          <MdLibraryBooks size={20} /> <p className="link-text">My Library</p>
+        </Link>
+        <Link className="link-list" to="/add-book">
+          <AiFillFileAdd size={20} /> <p className="link-text">Add Book</p>
+        </Link>
+      </div> */}
+      <br />
+      <hr />
+      <br />
+      <div style={{ marginLeft: "25%" }}>
+        <Link to="/" className="linkmenu" style={{ textDecoration: "none" }}>
+          <div
+            className="buttonmenu-logout"
+            onClick={() =>
+              dispatch({
+                type: "LOGOUT",
+              })
+            }
+          >
+            <div style={{ marginLeft: "5%" }}>
+              <RiLogoutBoxRFill size={24} />{" "}
+              <font className="namamenu"> Logout </font>
+            </div>
+          </div>
+        </Link>
+      </div>
+    </div>
+  ) : null;
 }
 
 export default Sidebar;
--- client/src/Components/Home/TopNavAdmin.jsx
+++ client/src/Components/Home/TopNavAdmin.jsx
@@ -3,44 +3,58 @@ import { Navbar, Dropdown } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { TiDocumentAdd } from "react-icons/ti";
 import { RiLogoutBoxRFill } from "react-icons/ri";
-import iconHeader from "../../Images/iconHeader.png";
-import fakePicture from "../../Dummy/Profile.json";
 import DropdownToggle from "react-bootstrap/esm/DropdownToggle";
+import { useQuery } from "react-query";
 import { LoginContext } from "../../Context/LoginContext";
+import { API, urlAsset } from "../../Config/api";
+import iconHeader from "../../Images/iconHeader.png";
+import Spinner from "../Spinner";
 
 function TopNavAdmin() {
   const [state, dispatch] = useContext(LoginContext);
-  return (
+  const id = localStorage.getItem("id");
+
+  const { isLoading, error, data: userData, refetch } = useQuery(
+    "getImage",
+    () => API.get(`/user/${state.user.id}`)
+  );
+
+  return isLoading || !userData ? (
+    <Spinner />
+  ) : error ? (
+    <h1>Your Error : {error.message}</h1>
+  ) : (
     <div>
       <div className="container-fluid d-flex justify-content-between ">
         <Link to="/home">
           <img src={iconHeader} alt="" />
-        <Navbar.Brand>
-          <iconHeader />
-        </Navbar.Brand>
+          <Navbar.Brand>
+            <iconHeader />
+          </Navbar.Brand>
         </Link>
 
         <Dropdown>
           <DropdownToggle variant="white">
-            {fakePicture.map((data) => (
-              <div id="adminNavbarProfilePicture">
-                <img
-                  className="adminNavbarProfilePicture"
-                  src={data.picture}
-                  alt=""
-                />
-              </div>
-            ))}
+            <div id="adminNavbarProfilePicture">
+              <img
+                className="adminNavbarProfilePicture"
+                src={urlAsset.img + userData.data.data.picture}
+                alt=""
+              />
+            </div>
           </DropdownToggle>
           <Dropdown.Menu>
             <Dropdown.Item>
-              <Link style={{ textDecoration: "none", color: "#929292" }} to="/addbook">
+              <Link
+                style={{ textDecoration: "none", color: "#929292" }}
+                to="/addbook"
+              >
                 <TiDocumentAdd /> Add Book
               </Link>
             </Dropdown.Item>
             <hr />
             <Dropdown.Item>
-              <Link 
+              <Link
                 style={{ textDecoration: "none", color: "#929292" }}
                 to="/"
                 onClick={() => dispatch({ type: "LOGOUT" })}
