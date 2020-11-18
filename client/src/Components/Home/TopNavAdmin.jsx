7dc67f6e664c429ffc33ec09879dcc314f401755
panzerstrike <emesha2030@tutanota.com>
2020-11-19 02:43:39 +0700

    delete unnecessary file

--- client/src/Components/Home/TopNavAdmin.jsx
+++ client/src/Components/Home/TopNavAdmin.jsx
@@ -1,23 +1,64 @@
 import React, { useContext } from "react";
-import { Navbar, Dropdown } from "react-bootstrap";
+import { Navbar, Dropdown, Modal, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { TiDocumentAdd } from "react-icons/ti";
 import { RiLogoutBoxRFill } from "react-icons/ri";
 import DropdownToggle from "react-bootstrap/esm/DropdownToggle";
-import { useQuery } from "react-query";
+import { useMutation, useQuery } from "react-query";
 import { LoginContext } from "../../Context/LoginContext";
 import { API, urlAsset } from "../../Config/api";
 import iconHeader from "../../Images/iconHeader.png";
 import Spinner from "../Spinner";
+import { useState } from "react";
 
 function TopNavAdmin() {
   const [state, dispatch] = useContext(LoginContext);
   const id = localStorage.getItem("id");
 
+  const [showAddCategory, setShowAddCategory] = useState(false);
+
+  const handleShowCategory = () => setShowAddCategory(true);
+  const handleCloseCategory = () => setShowAddCategory(false);
+
   const { isLoading, error, data: userData, refetch } = useQuery(
     "getImage",
     () => API.get(`/user/${state.user.id}`)
   );
+
+  const [formData, setFormData] = useState({
+    name: "",
+  });
+
+  const { name } = formData;
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const [addCategory] = useMutation(async () => {
+    try {
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+
+      const body = JSON.stringify({
+        name,
+      });
+
+      const res = await API.post("/category", formData, config);
+
+      setFormData({
+        name: "",
+      });
+
+      setShowAddCategory(true);
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  });
 
   return isLoading || !userData ? (
     <Spinner />
@@ -52,6 +93,15 @@ function TopNavAdmin() {
                 <TiDocumentAdd /> Add Book
               </Link>
             </Dropdown.Item>
+            <Dropdown.Item>
+              <Link
+                style={{ textDecoration: "none", color: "#929292" }}
+                // to="/admin-add-category"
+                onClick={handleShowCategory}
+              >
+                <TiDocumentAdd /> Add Category
+              </Link>
+            </Dropdown.Item>
             <hr />
             <Dropdown.Item>
               <Link
@@ -65,6 +115,28 @@ function TopNavAdmin() {
           </Dropdown.Menu>
         </Dropdown>
       </div>
+
+      <Modal centered show={showAddCategory} onHide={handleCloseCategory}>
+        <Modal.Body>
+          <h1>Add Category</h1>
+          <Form
+            onSubmit={(e) => {
+              e.preventDefault();
+              addCategory();
+            }}
+          >
+            <Form.Group>
+              <Form.Control
+                type="text"
+                placeholder="Name"
+                name="name"
+                value={name}
+                onChange={(e) => handleChange(e)}
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+      </Modal>
     </div>
   );
 }
